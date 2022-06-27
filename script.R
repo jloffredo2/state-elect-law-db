273e88bff0f040ae64581c7a11318302299f73bc
Joe Loffredo <joseph.r.loffredo@gmail.com>
2022-06-27 17:08:43 -0400

    gets topic cols

--- script.R
+++ script.R
@@ -2,6 +2,7 @@ rm(list = ls())
 
 library(tidyverse)
 library(rvest)
+library(rjson)
 
 splitAt <- function(x, pos) {
   out <- list()
@@ -14,6 +15,7 @@ splitAt <- function(x, pos) {
   }
   return(out)
 }
+
 extract_bill_info <- function(curr, year) {
   bill_id = curr[1]
   print(bill_id)
@@ -50,6 +52,10 @@ extract_bill_info <- function(curr, year) {
       history = toJSON(history)
     )
   )
+}
+
+check_topics <- function(json,topic){
+  return(ifelse(sum(str_detect(fromJSON(json),topic))>0,1,0))
 }
 
 bill_database <- data.frame()
@@ -105,95 +111,96 @@ bill_database$author_name <- ifelse(str_detect(bill_database$author,"\\([A-Z]{1,
                                     trimws(str_remove_all(bill_database$author,"\\([A-Z]{1,3}\\)"),"both"),
                                     bill_database$author)
 bill_database$author_party <- str_remove_all(str_extract(bill_database$author,"\\([A-Z]{1,3}\\)"),"[()]")
-# Use open states to get leg info
-
-categories <- c(
-"Absentee Voting-Application for"
-,"Absentee Voting-Distributing Ballots"
-,"Absentee Voting-Early Voting/In-Person Absentee"
-,"Absentee Voting-Eligibility"
-,"Absentee Voting-Military/Overseas"
-,"Absentee Voting-Misc."
-,"Absentee Voting-MOVE Act"
-,"Absentee Voting-No Excuse"
-,"Absentee Voting-Permanent Status"
-,"Absentee Voting-Returning Ballots"
-,"Alt Voting Methods (Ranked Choice, etc)"
-,"Audits-Post Election"
-,"Ballot Access-Candidates"
-,"Ballot Access-Parties"
-,"Ballots-Format & Design"
-,"Candidates-Qualifications for Office"
-,"Candidates-Resign to Run"
-,"Candidates-Withdrawal/Death"
-,"Candidates-Write-in"
-,"Challenges to Voters"
-,"Contests"
-,"Cost of Elections"
-,"Counting Votes"
-,"Cybersecurity"
-,"Dates of Elections"
-,"DREs-Paper Trail"
-,"Election Crimes"
-,"Election Data-Collection/Retention of"
-,"Election Day Holiday"
-,"Election Officials-Campaign Activities"
-,"Election Officials-Local"
-,"Election Officials-Statewide"
-,"Election Results/Canvass, Reporting of"
-,"Electioneering"
-,"Electoral College"
-,"Electoral College-National Popular Vote"
-,"Emergencies/Disasters"
-,"Exit Polling"
-,"Fusion/Dual-Party"
-,"Internet Voting"
-,"Mail Voting"
-,"Miscellaneous"
-,"Offices-Method of Filling"
-,"Political Parties"
-,"Poll Watchers"
-,"Poll Workers-Compensation"
-,"Poll Workers-Misc."
-,"Poll Workers-Selection/Qualifications of"
-,"Poll Workers-Training"
-,"Poll Workers-Youth"
-,"Polling Places-Arrangement of/Procedures at"
-,"Polling Places-Disabled Access"
-,"Polling Places-Hours"
-,"Polling Places-Locations"
-,"Polling Places-Vote Centers"
-,"Precinct Definition"
-,"Primaries-Dates"
-,"Primaries-Misc."
-,"Primaries-Presidential"
-,"Primaries-Runoff"
-,"Primaries-Types"
-,"Provisional Voting"
-,"Recounts"
-,"Registration Drives"
-,"Registration-Application Form/Content"
-,"Registration-Automatic"
-,"Registration-Centralized Voter List"
-,"Registration-Deadline"
-,"Registration-Election Day"
-,"Registration-Electronic"
-,"Registration-ID Required"
-,"Registration-List Maintenance"
-,"Registration-Misc."
-,"Registration-Preregistration"
-,"Registration-Sale/Distribution/Use of Lists"
-,"Run-Off Elections"
-,"Special Elections"
-,"Straight Ticket Voting"
-,"Task Forces/Study Commissions/Interim Committees"
-,"Vacancies"
-,"Voter Education/Information"
-,"Voter Identification"
-,"Voters-Absence from Work"
-,"Voters-Age"
-,"Voters-Assistance to"
-,"Voters-Felon Voting Rights"
-,"Voters-Miscellaneous Qualifications"
-,"Voting Equipment/Technology-Selection & Standards"
-,"Voting System Testing/Security/Storage")
+# Count cosponsors
+bill_database$coauthors[bill_database$coauthors=="\"NA\""] = NA
+# Categorize wide
+#bill_database$ELCOST = sapply(bill_database$topics,check_topics,"Cost of Elections")
+bill_database$AVAPPL = sapply(bill_database$topics, check_topics, "Absentee Voting-Application for")
+bill_database$AVBDIS = sapply(bill_database$topics, check_topics, "Absentee Voting-Distributing Ballots")
+bill_database$AVEVIP = sapply(bill_database$topics, check_topics, "Absentee Voting-Early Voting/In-Person Absentee")
+bill_database$AVELIG = sapply(bill_database$topics, check_topics, "Absentee Voting-Eligibility")
+bill_database$AVMIOV = sapply(bill_database$topics, check_topics, "Absentee Voting-Military/Overseas")
+bill_database$AVMISC = sapply(bill_database$topics, check_topics, "Absentee Voting-Misc.")
+bill_database$AVMOVE = sapply(bill_database$topics, check_topics, "Absentee Voting-MOVE Act")
+bill_database$AVNOEX = sapply(bill_database$topics, check_topics, "Absentee Voting-No Excuse")
+bill_database$AVPERM = sapply(bill_database$topics, check_topics, "Absentee Voting-Permanent Status")
+bill_database$AVBRET = sapply(bill_database$topics, check_topics, "Absentee Voting-Returning Ballots")
+bill_database$VOTEME = sapply(bill_database$topics, check_topics, "Alt Voting Methods (Ranked Choice, etc)")
+bill_database$AUDITS = sapply(bill_database$topics, check_topics, "Audits-Post Election")
+bill_database$BACAND = sapply(bill_database$topics, check_topics, "Ballot Access-Candidates")
+bill_database$BAPART = sapply(bill_database$topics, check_topics, "Ballot Access-Parties")
+bill_database$BALDES = sapply(bill_database$topics, check_topics, "Ballots-Format & Design")
+bill_database$CANQUL = sapply(bill_database$topics, check_topics, "Candidates-Qualifications for Office")
+bill_database$CANRTR = sapply(bill_database$topics, check_topics, "Candidates-Resign to Run")
+bill_database$CANWDW = sapply(bill_database$topics, check_topics, "Candidates-Withdrawal/Death")
+bill_database$CANWRI = sapply(bill_database$topics, check_topics, "Candidates-Write-in")
+bill_database$VTRCHA = sapply(bill_database$topics, check_topics, "Challenges to Voters")
+bill_database$CNTEST = sapply(bill_database$topics, check_topics, "Contests")
+bill_database$ELCOST = sapply(bill_database$topics, check_topics, "Cost of Elections")
+bill_database$VCOUNT = sapply(bill_database$topics, check_topics, "Counting Votes")
+bill_database$CYBSEC = sapply(bill_database$topics, check_topics, "Cybersecurity")
+bill_database$ELDATE = sapply(bill_database$topics, check_topics, "Dates of Elections")
+bill_database$PTDRES = sapply(bill_database$topics, check_topics, "DREs-Paper Trail")
+bill_database$CRIMES = sapply(bill_database$topics, check_topics, "Election Crimes")
+bill_database$DATART = sapply(bill_database$topics, check_topics, "Election Data-Collection/Retention of")
+bill_database$EDHOLI = sapply(bill_database$topics, check_topics, "Election Day Holiday")
+bill_database$EOCAMP = sapply(bill_database$topics, check_topics, "Election Officials-Campaign Activities")
+bill_database$EOLOCA = sapply(bill_database$topics, check_topics, "Election Officials-Local")
+bill_database$EOSTWD = sapply(bill_database$topics, check_topics, "Election Officials-Statewide")
+bill_database$REPRES = sapply(bill_database$topics, check_topics, "Election Results/Canvass, Reporting of")
+bill_database$ELEING = sapply(bill_database$topics, check_topics, "Electioneering")
+bill_database$ELECOL = sapply(bill_database$topics, check_topics, "Electoral College")
+bill_database$ECONPV = sapply(bill_database$topics, check_topics, "Electoral College-National Popular Vote")
+bill_database$EMEDIS = sapply(bill_database$topics, check_topics, "Emergencies/Disasters")
+bill_database$EXPOLL = sapply(bill_database$topics, check_topics, "Exit Polling")
+bill_database$DUALFU = sapply(bill_database$topics, check_topics, "Fusion/Dual-Party")
+bill_database$INVOTE = sapply(bill_database$topics, check_topics, "Internet Voting")
+bill_database$MAILVO = sapply(bill_database$topics, check_topics, "Mail Voting")
+bill_database$MISCEL = sapply(bill_database$topics, check_topics, "Miscellaneous")
+bill_database$FILING = sapply(bill_database$topics, check_topics, "Offices-Method of Filling")
+bill_database$POLPAR = sapply(bill_database$topics, check_topics, "Political Parties")
+bill_database$POLWAT = sapply(bill_database$topics, check_topics, "Poll Watchers")
+bill_database$PWCOMP = sapply(bill_database$topics, check_topics, "Poll Workers-Compensation")
+bill_database$PWMISC = sapply(bill_database$topics, check_topics, "Poll Workers-Misc.")
+bill_database$PWQUAL = sapply(bill_database$topics, check_topics, "Poll Workers-Selection/Qualifications of")
+bill_database$PWTRAI = sapply(bill_database$topics, check_topics, "Poll Workers-Training")
+bill_database$PWYOTH = sapply(bill_database$topics, check_topics, "Poll Workers-Youth")
+bill_database$PPPROC = sapply(bill_database$topics, check_topics, "Polling Places-Arrangement of/Procedures at")
+bill_database$PPACES = sapply(bill_database$topics, check_topics, "Polling Places-Disabled Access")
+bill_database$PPVHRS = sapply(bill_database$topics, check_topics, "Polling Places-Hours")
+bill_database$PPLOCA = sapply(bill_database$topics, check_topics, "Polling Places-Locations")
+bill_database$PPVCEN = sapply(bill_database$topics, check_topics, "Polling Places-Vote Centers")
+bill_database$PREDEF = sapply(bill_database$topics, check_topics, "Precinct Definition")
+bill_database$PRIDAT = sapply(bill_database$topics, check_topics, "Primaries-Dates")
+bill_database$PRIMIS = sapply(bill_database$topics, check_topics, "Primaries-Misc.")
+bill_database$PRIPUS = sapply(bill_database$topics, check_topics, "Primaries-Presidential")
+bill_database$PRIRNF = sapply(bill_database$topics, check_topics, "Primaries-Runoff")
+bill_database$PRITYP = sapply(bill_database$topics, check_topics, "Primaries-Types")
+bill_database$PROVOT = sapply(bill_database$topics, check_topics, "Provisional Voting")
+bill_database$RECOUN = sapply(bill_database$topics, check_topics, "Recounts")
+bill_database$REGDRI = sapply(bill_database$topics, check_topics, "Registration Drives")
+bill_database$REGAPP = sapply(bill_database$topics, check_topics, "Registration-Application Form/Content")
+bill_database$REGATO = sapply(bill_database$topics, check_topics, "Registration-Automatic")
+bill_database$REGCVL = sapply(bill_database$topics, check_topics, "Registration-Centralized Voter List")
+bill_database$REGDTE = sapply(bill_database$topics, check_topics, "Registration-Deadline")
+bill_database$REGEDY = sapply(bill_database$topics, check_topics, "Registration-Election Day")
+bill_database$REGELE = sapply(bill_database$topics, check_topics, "Registration-Electronic")
+bill_database$REGIDR = sapply(bill_database$topics, check_topics, "Registration-ID Required")
+bill_database$REGLST = sapply(bill_database$topics, check_topics, "Registration-List Maintenance")
+bill_database$REGMSC = sapply(bill_database$topics, check_topics, "Registration-Misc.")
+bill_database$REGPRE = sapply(bill_database$topics, check_topics, "Registration-Preregistration")
+bill_database$REGSDL = sapply(bill_database$topics, check_topics, "Registration-Sale/Distribution/Use of Lists")
+bill_database$RUNOFF = sapply(bill_database$topics, check_topics, "Run-Off Elections")
+bill_database$SPELEC = sapply(bill_database$topics, check_topics, "Special Elections")
+bill_database$STVOTE = sapply(bill_database$topics, check_topics, "Straight Ticket Voting")
+bill_database$TFSCIC = sapply(bill_database$topics, check_topics, "Task Forces/Study Commissions/Interim Committees")
+bill_database$VACNCY = sapply(bill_database$topics, check_topics, "Vacancies")
+bill_database$VEDINF = sapply(bill_database$topics, check_topics, "Voter Education/Information")
+bill_database$VOTRID = sapply(bill_database$topics, check_topics, "Voter Identification")
+bill_database$VOTAFW = sapply(bill_database$topics, check_topics, "Voters-Absence from Work")
+bill_database$VOTAGE = sapply(bill_database$topics, check_topics, "Voters-Age")
+bill_database$VOTAST = sapply(bill_database$topics, check_topics, "Voters-Assistance to")
+bill_database$VOTFVR = sapply(bill_database$topics, check_topics, "Voters-Felon Voting Rights")
+bill_database$VOTMQU = sapply(bill_database$topics, check_topics, "Voters-Miscellaneous Qualifications")
+bill_database$TECHSS = sapply(bill_database$topics, check_topics, "Voting Equipment/Technology-Selection & Standards")
+bill_database$VSSCST = sapply(bill_database$topics, check_topics, "Voting System Testing/Security/Storage")
