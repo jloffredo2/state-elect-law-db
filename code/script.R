6b984b8d41a68f7dfdcb7e38bdaf0c4606d498cd
Joe Loffredo <joseph.r.loffredo@gmail.com>
2022-07-18 11:07:20 -0400

    updates codebook

--- code/script.R
+++ code/script.R
@@ -26,9 +26,11 @@ save(vrl_bill_database, ncsl_bill_database, file = "output/state_elect_law_db.Rd
 codebook = readLines("../output/codebook.txt")
 codebook[4] = sprintf("Date: %s",today())
 codebook[10] = sprintf("  National Conference of State Legislatures. 2022. *State Elections Legislation Database*. www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx (%s)",(stamp("March 1, 1999")(today())))
+codebook[12] = sprintf("  Voting Rights Lab. 2022. *Legislative Tracker*. https://tracker.votingrightslab.org/pending/search (%s)",(stamp("March 1, 1999")(today())))
 writeLines(codebook, "../output/codebook.txt")
 
 readme = readLines("../output/README.md")
 readme[6] = sprintf("**Date**: %s", today())
 readme[13] = sprintf("> National Conference of State Legislatures. 2022. *State Elections Legislation Database*. www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx (%s).",(stamp("March 1, 1999")(today())))
+readme[15] = sprintf("> Voting Rights Lab. 2022. *Legislative Tracker*. https://tracker.votingrightslab.org/pending/search (%s).",(stamp("March 1, 1999")(today())))
 writeLines(readme, "../output/README.md")
