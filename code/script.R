e43da22ef62a92c726d25276d06bb6209807a413
Joe Loffredo <joseph.r.loffredo@gmail.com>
2022-07-18 12:51:07 -0400

    repath codebook and readme changes

--- code/script.R
+++ code/script.R
@@ -23,14 +23,14 @@ save(vrl_bill_database, file = "output/vrl_bill_database.Rdata")
 save(vrl_bill_database, ncsl_bill_database, file = "output/state_elect_law_db.Rdata")
 
 # Edit codebook and readme
-codebook = readLines("../output/codebook.txt")
+codebook = readLines("output/codebook.txt")
 codebook[4] = sprintf("Date: %s",today())
 codebook[10] = sprintf("  National Conference of State Legislatures. 2022. *State Elections Legislation Database*. www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx (%s)",(stamp("March 1, 1999")(today())))
 codebook[12] = sprintf("  Voting Rights Lab. 2022. *Legislative Tracker*. https://tracker.votingrightslab.org/pending/search (%s)",(stamp("March 1, 1999")(today())))
-writeLines(codebook, "../output/codebook.txt")
+writeLines(codebook, "output/codebook.txt")
 
-readme = readLines("../output/README.md")
+readme = readLines("output/README.md")
 readme[6] = sprintf("**Date**: %s", today())
 readme[13] = sprintf("> National Conference of State Legislatures. 2022. *State Elections Legislation Database*. www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx (%s).",(stamp("March 1, 1999")(today())))
 readme[15] = sprintf("> Voting Rights Lab. 2022. *Legislative Tracker*. https://tracker.votingrightslab.org/pending/search (%s).",(stamp("March 1, 1999")(today())))
-writeLines(readme, "../output/README.md")
+writeLines(readme, "output/README.md")
