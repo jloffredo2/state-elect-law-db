ab76b583c009c5bbb0122f8b3c94740ff1480827
Joe Loffredo <joseph.r.loffredo@gmail.com>
2022-07-20 00:08:46 -0400

    ballotpedia formatting changes

--- code/ballotpedia.R
+++ code/ballotpedia.R
@@ -1,4 +1,4 @@
-scrape_billtrack_sponsors <- function(link){
+scrape_billtrack_data <- function(link){
   print(link)
   author_list <- head(lapply(gsub(
     "[\r\n]", "",
@@ -8,26 +8,59 @@ scrape_billtrack_sponsors <- function(link){
       str_trim("both")
   ) %>% str_split(","),str_trim, "both")[[1]], - 1)
   
-  authors <- author_list[unlist(lapply(author_list,str_detect,"\\*"))]
-  cosponsors <- author_list[!author_list %in% authors]
+  INTRODUCEDDATE <- mdy(
+    read_html(link) %>%
+      html_element('.current:nth-child(1)') %>%
+      html_text() %>%
+      str_trim("both") %>%
+      str_remove("Introduced\r\n")
+  )
+  
+  AUTHOR <-
+    author_list[unlist(lapply(author_list, str_detect, "\\*"))]
+  COAUTHORS <- author_list[!author_list %in% authors]
   
   NCOAUTHORS <- length(cosponsors)
-  NDEMCOAUTHORS <- sum(str_detect(cosponsors,"\\(D\\)")) + sum(str_detect(cosponsors,"\\(DFL\\)"))
-  NREPCOAUTHORS <- sum(str_detect(cosponsors,"\\(R\\)"))
+  NDEMCOAUTHORS <- sum(str_detect(cosponsors, "\\(D\\)")) + sum(str_detect(cosponsors, "\\(DFL\\)"))
+  NREPCOAUTHORS <- sum(str_detect(cosponsors, "\\(R\\)"))
   
-  return(data.frame(bill_track_link=link,NCOAUTHORS=NCOAUTHORS,NDEMCOAUTHORS=NDEMCOAUTHORS,NREPCOAUTHORS=NREPCOAUTHORS))
+  return(
+    data.frame(
+      bill_track_link = link,
+      AUTHORNAME = rjson::toJSON(AUTHOR),
+      COAUTHORS = rjson::toJSON(COAUTHORS),
+      NCOAUTHORS = NCOAUTHORS,
+      NDEMCOAUTHORS = NDEMCOAUTHORS,
+      NREPCOAUTHORS = NREPCOAUTHORS,
+      INTRODUCEDDATE = INTRODUCEDDATE
+    )
+  )
 }
 
 build_ballotpedia_bill_database <- function(){
   ballotpedia_initial = read.csv("output/ballotpedia_initial.csv")
-  sponsor_data = lapply(unique(ballotpedia_initial$bill_track_link), scrape_billtrack_sponsors)
+  sponsor_data = lapply(unique(ballotpedia_initial$bill_track_link), scrape_billtrack_data)
   
   ballotpedia_scraped <- bind_rows(
     map(sponsor_data, function(x){merge(ballotpedia_initial,x,by="bill_track_link")}),
     .id = 'id'
   )
   
-  save(ballotpedia_scraped, file = "output/ballotpedia_scraped.RData")
+  #save(ballotpedia_scraped, file = "output/ballotpedia_scraped.RData")
   
+  load("output/ballotpedia_scraped.RData")
+  colnames(ballotpedia_scraped) <- str_to_upper(colnames(ballotpedia_scraped))
+  ballotpedia_bill_database <- ballotpedia_scraped %>%
+    rename(YEAR = SESSION_YEAR
+           ,BILLSTATUS = CURRENT_LEGISLATIVE_STATUS
+           ,BILLNUM = BILL_NUMBER
+           ,LASTACTIONDATE = ACTION_DATE
+           ,BILLSUMMARY = SUMMARY
+           ,BILLTEXTURL = BILL_TRACK_LINK) %>%
+    mutate(LASTACTIONDATE = ymd(LASTACTIONDATE)
+           ,CATEGORIES = str_split(CATEGORIES, ",")
+           ,BILLNUM = str_replace_all(BILLNUM,c("HB"='H','SB'='S'))
+           ,UUID = str_c(STATE,YEAR,BILLNUM))
+    
   return(ballotpedia_scraped)
 }
