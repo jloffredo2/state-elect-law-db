d85e1df58786928769636ad2ec69335413ab7037
Joe Loffredo <60896458+jloffredo2@users.noreply.github.com>
2022-07-25 14:08:06 -0400

    Update update_documentation.R
    fixed missing =

--- code/update_documentation.R
+++ code/update_documentation.R
@@ -2,7 +2,7 @@ library(lubridate)
 
 # Edit codebook and readme
 readme = readLines("README.md")
-readme[1] sprintf("# STATE ELECTIONS LEGISLATION DATABASE (2011-%i)",year(today()))
+readme[1] = sprintf("# STATE ELECTIONS LEGISLATION DATABASE (2011-%i)",year(today()))
 readme[6] = sprintf("**Date**: %s", today())
 readme[13] = sprintf("> National Conference of State Legislatures. 2022. *State Elections Legislation Database*. www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx (%s).",(stamp("March 1, 1999")(today())))
 readme[15] = sprintf("> Voting Rights Lab. 2022. *Legislative Tracker*. https://tracker.votingrightslab.org/pending/search (%s).",(stamp("March 1, 1999")(today())))
