04b0ae1370de9f0359c1cd061fd4dcfbdbed5363
Joe Loffredo <joseph.r.loffredo@gmail.com>
2023-07-01 20:44:22 -0400

    remove ballotpedia, update documentation

--- code/script.R
+++ code/script.R
@@ -13,12 +13,10 @@ library(purrr)
 source("code/misc_fxns.R")
 source("code/ncsl.R")
 source("code/vrl.R")
-source("code/ballotpedia.R")
 
 # Build scraped datasets
 ncsl_bill_database <- build_ncsl_bill_database()
 vrl_bill_database <- build_vrl_bill_database()
-ballotpedia_bill_database <- build_ballotpedia_bill_database()
 
 # Save outputs
 write.csv(ncsl_bill_database, file = "output/ncsl_bill_database.csv",row.names = FALSE)
@@ -26,6 +24,3 @@ save(ncsl_bill_database, file = "output/ncsl_bill_database.Rdata")
 
 write.csv(vrl_bill_database, file = "output/vrl_bill_database.csv",row.names = FALSE)
 save(vrl_bill_database, file = "output/vrl_bill_database.Rdata")
-
-write.csv(ballotpedia_bill_database, file = "ballotpedia/vrl_bill_database.csv",row.names = FALSE)
-save(ballotpedia_bill_database, file = "output/ballotpedia_bill_database.Rdata")
